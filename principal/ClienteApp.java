d2fd839f8610e001d9f9bebec62a1c71c5f6a685
yorranancosta <yorranan.almeida@gmail.com>
2022-09-20 16:29:53 -0300

    Mudanca no App, acrescentado while e console

--- principal/ClienteApp.java
+++ principal/ClienteApp.java
@@ -1,3 +1,12 @@
+import entradados.*;
+
 public class ClienteApp {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    int count = 0; 
+
+    while (count < 10) {
+      Console.ler();
+      count++;
+    }
+  }
 }
