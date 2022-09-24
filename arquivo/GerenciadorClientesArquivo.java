e704083b94b586c574b4f600e46ef589c0666ee5
Felipe JS <83662671+SpecterInAHusk@users.noreply.github.com>
2022-09-24 15:32:07 -0300

    Adicionados corpos dos métodos das classes arquivo

--- arquivo/GerenciadorClientesArquivo.java
+++ arquivo/GerenciadorClientesArquivo.java
@@ -1,8 +1,34 @@
 package arquivo;
+import java.io.*;
 
 public class GerenciadorClientesArquivo implements GerenciadorClientes {
 
-  public void lerClientes() {}
+    public void gravarClientes(String nome) {BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter("clientes.txt", true));
+			writer.write(nome);
+			writer.newLine();
+		} catch (IOException exc) {
+			System.out.println("Erro de I/O" + exc);
+		} finally {
+			try {
+				if (writer != null)
+					writer.close();
+			} catch (IOException exc) {
+				System.out.println("Fechando arquivo clientes.txt" + exc);
+			}
+		}}
 
-  public void gravarClientes(String nome) {}
+    public void lerClientes() {
+        String s;
+
+        // Cria e usa um FileReader encapsulado em um BufferedReader.
+        try (BufferedReader br = new BufferedReader(new FileReader("cleintes.txt"))) {
+            while ((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (IOException exc) {
+            System.out.println("Erro de I/O: " + exc);
+        }
+    }
 }
